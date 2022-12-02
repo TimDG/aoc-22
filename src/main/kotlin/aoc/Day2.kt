32efa405a2eaed99a35c302e3366d7c964305003
Tim DG <github@de-grande.be>
2022-12-02 10:06:27 +0100

    Advent of code day 2 - optimize day 2

--- src/main/kotlin/aoc/Day2.kt
+++ src/main/kotlin/aoc/Day2.kt
@@ -2,57 +2,37 @@ package aoc
 
 import java.io.File
 
-class Day2(input: String) {
-    private val matches: List<Match>
-    init {
-        matches = input.lines()
-            .map { Match(it) }
-    }
-
-    fun simpleScore(): Int {
-        return matches.sumOf { it.simpleScore() }
-    }
-
-    fun optimalScore(): Int {
-        return matches.sumOf { it.optimalScore() }
-    }
+private fun optimal(s: String) = when (s) {
+    "A X" -> 0 + 3
+    "A Y" -> 3 + 1
+    "A Z" -> 6 + 2
+    "B X" -> 0 + 1
+    "B Y" -> 3 + 2
+    "B Z" -> 6 + 3
+    "C X" -> 0 + 2
+    "C Y" -> 3 + 3
+    "C Z" -> 6 + 1
+    else -> throw IllegalStateException()
 }
 
-class Match(private val desc: String) {
-    fun simpleScore(): Int {
-        return when (desc) {
-            "A X" -> 1 + 3
-            "A Y" -> 2 + 6
-            "A Z" -> 3 + 0
-            "B X" -> 1 + 0
-            "B Y" -> 2 + 3
-            "B Z" -> 3 + 6
-            "C X" -> 1 + 6
-            "C Y" -> 2 + 0
-            "C Z" -> 3 + 3
-            else -> throw IllegalStateException()
-        }
-    }
-
-    fun optimalScore(): Int {
-        return when (desc) {
-            "A X" -> 0+3
-            "A Y" -> 3+1
-            "A Z" -> 6+2
-            "B X" -> 0+1
-            "B Y" -> 3+2
-            "B Z" -> 6+3
-            "C X" -> 0+2
-            "C Y" -> 3+3
-            "C Z" -> 6+1
-            else -> throw IllegalStateException()
-        }
-    }
+private fun simple(s: String) = when (s) {
+    "A X" -> 1 + 3
+    "A Y" -> 2 + 6
+    "A Z" -> 3 + 0
+    "B X" -> 1 + 0
+    "B Y" -> 2 + 3
+    "B Z" -> 3 + 6
+    "C X" -> 1 + 6
+    "C Y" -> 2 + 0
+    "C Z" -> 3 + 3
+    else -> throw IllegalStateException()
 }
 
 fun main() {
     val input = File("inputs/day2.txt").readText()
-    val day2 = Day2(input)
-    println(day2.simpleScore())
-    println(day2.optimalScore())
+
+    val lines = input.lines()
+    println(lines.sumOf { simple(it) })
+    println(lines.sumOf { optimal(it) })
 }
+
